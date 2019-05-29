d3856b1428894843f2c80f27661ca6fb86ded878
Ivars <ivars.saudinis@liepu.lv>
2019-05-29 16:43:46 +0300

    nepabeigts..

---
+++ steks-a-b.cpp
@@ -0,0 +1,148 @@
+ /*
+  * 1.Uzrakstīt C++ programmu, kas, izmantojot steku, nosaka, vai 
+  * simbolu virknē ir vienāds burtu A un B skaits.
+  * 
+  * Algoritms gadījumam,   * kad A skaits ir lielāks nekā B: ja 
+  * nolasa A, stekā ieliek 1, ja * nolasa B, no steka 1 izņem; 
+  * ja programmas beigās steks ir tukšs, * tātad = nolasīto A un B skaits ir vienāds.
+  * -- 
+  * build with Geany & gcc version 6.3.0 20170516 (Debian 6.3.0-18+deb9u1) 
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+struct Steks
+{
+	int Data;
+	Steks *Next;
+};
+
+void Create(Steks* &S);
+void Push(Steks* &S, int D);
+void Pop(Steks* &S);
+int  Top(Steks* &S);
+bool isEmpty(Steks* &S);
+
+// majasdarba funkcijas
+int Lielums (Steks * &S);
+void Druka (Steks * &S);
+void ReversaDruka (Steks * &S);
+int Ntais (Steks* &S, int N); 
+
+int main()
+{
+	Steks *StA; srand(time(0)); int Num;
+		
+	cout << "Steks A:" << endl;
+	Create(StA);
+	for (int i=0; i<15; i++)
+	{
+		Num = rand() % 99 + 1;
+		cout << Num << " ";
+		Push(StA, Num);	 
+	}
+	  
+	 cout << endl;
+	 cout << "Lielums: " << Lielums(StA) << endl;
+	
+	 cout <<  "Druka: "; Druka(StA);  cout << endl;
+	 cout <<  "ReversaDruka: "; ReversaDruka(StA); cout << endl;
+	 cout <<  "4. elements: " <<  Ntais(StA, 4);
+	  
+	return 0;
+}
+
+int Lielums (Steks* &S)
+{
+	Steks *p;
+	p = S;
+	int skaits=0;
+	while( p != NULL)
+	{		
+		p = p->Next; 
+		skaits++; 
+	} 
+	return skaits;
+}
+
+void Druka (Steks * &S)
+{
+	Steks *p;
+	p = S;
+	while( p != NULL)
+	{
+		cout << p->Data << " ";
+		p = p->Next; 
+	}
+}
+
+void ReversaDruka (Steks * &S)
+{
+	Steks *p, *c;
+	p = S;
+	c = new Steks;
+	while( p != NULL)
+	{
+		Push(c,p->Data);
+		p = p->Next; 
+	}
+	  
+	  Druka(c);
+	
+}
+
+int Ntais (Steks* &S, int N)
+{
+	Steks *p; p = S;
+	
+	int c = 0;
+	while( p != NULL)
+	{
+		if(c==N)
+		{
+			return p->Data;
+		}
+		p = p->Next; 
+		c++;
+	}
+	return 0;
+}
+
+void Create(Steks* &S)
+{ S = NULL; }
+
+void Push(Steks* &S, int D)
+{
+	if (S == NULL)
+	{
+		S = new Steks;
+		S->Data = D; S->Next = NULL;
+	}
+	else
+	{
+		Steks *p; p = new Steks;
+		p->Data = D; p->Next = S; S = p;
+	}
+}
+
+void Pop(Steks* &S)
+{
+	if (S != NULL)
+	{
+		Steks *p; p = S;
+		S = S->Next; 
+		delete p;
+	}
+}
+
+int  Top(Steks* &S)
+{
+	return (S==NULL) ? -1 : S->Data;
+}
+bool isEmpty(Steks* &S)
+{
+	return (S==NULL) ? true : false;
+}
